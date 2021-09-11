8c0e92346eebf79e80129a0af6441fb71b3767fc
Francisca <franciscaljubetic@gmail.com>
2021-09-11 04:45:20 -0300

    sin borrar los ya cancelados, falta en un texto en los iconos

--- main/models.py
+++ main/models.py
@@ -21,7 +21,7 @@ class UserManager(models.Manager):
         if not SOLO_LETRAS.match(postData['name']):
             errors['solo_letras'] = "solo letras en nombreporfavor"
 
-        if len(postData['password']) < 4:
+        if len(postData['password']) < 8:
             errors['password'] = "contraseña debe tener al menos 8 caracteres";
 
         if postData['password'] != postData['password_confirm'] :
@@ -30,25 +30,25 @@ class UserManager(models.Manager):
         
         return errors
     
-'''   
+
 class travelmanager(models.Manager):
     def basic_validator(self, post_data):
         errors ={}
         #today = date.today()
         if len(post_data['destination']) < 2: 
-            errors['destination'] = 'IGDAF'
+            errors['destination'] = 'IGDAF, write a decent title'
 
-        if len(post_data['description']) < 2:
-            errors['description'] = 'Title should have at least two characters'
+        if len(post_data['description']) < 15:
+            errors['description'] = 'Description should have at least fifteen characters'
         
-        if datetime.strptime(post_data['travel_from'],"%Y-%m-%d").date() < datetime.strptime(post_data['travel_to'],"%Y-%m-%d").date():
+        if datetime.strptime(post_data['travel_from'],"%Y-%m-%d").date() > datetime.strptime(post_data['travel_to'],"%Y-%m-%d").date():
             errors['travel_to'] = 'Better Talk to Marty Mc Fly'
             
         if datetime.strptime(post_data['travel_from'],"%Y-%m-%d").date() < datetime.today().date():
             errors['travel_from'] = 'Your departure should be later than today'
             
         return errors 
-'''
+
 
 class User(models.Model):
     CHOICES = (
@@ -78,7 +78,8 @@ class Travel(models.Model):
     creator = models.ForeignKey(User, related_name="created_travels", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    travellers = models.ManyToManyField(User, related_name='travels')
+    travellers = models.ManyToManyField(User, related_name='travels') 
+    objects = travelmanager()
     
     
     def __str__(self):
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ import bcrypt
 from main.decorators import login_required
 from main.models import User, Travel
 from django.db import IntegrityError
+from django.db.models import Q
 
 
 @login_required
@@ -14,85 +15,101 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+@login_required
 def travels(request):
-    travels = Travel.objects.all()
-    you_id = request.session['user']['id']
-    you = User.objects.get(id = you_id)
-    your_travels = Travel.objects.filter (travellers = you)
-    your_created_travels = Travel.objects.filter (creator = you)
-    your_travels_travellers = Travel.objects.filter(creator = you).exclude(travellers = you)
-    other_users_travels = [trip for trip in Travel.objects.all() if trip.id not in your_travels]
+    your_id = request.session['user']['id']
+    you = User.objects.get(id = your_id)
+    your_total_travels = Travel.objects.filter(Q(creator = you) | Q(travellers = you)) 
+    other_users_travels = [trip for trip in Travel.objects.all() if trip.id not in your_total_travels]
     context = {
-        'your_travels': your_travels,
-        'your_created_travels':your_created_travels,
         'other_users_travels': other_users_travels,
-        'your_travels_travellers': your_travels_travellers,
+        'your_total_travels': your_total_travels,
     }
     return render(request, 'travels.html', context)
 
+@login_required
 def travelsadd(request):
-    #errors = Travel.objects.basic_validator(request.POST)
     if request.method == 'GET':
     
         return render(request, 'travelsadd.html')
     
     else:
-        destination= request.POST['destination']
+        desti= request.POST['destination']
         description =request.POST['description']
         travel_from =request.POST['travel_from']
         travel_to = request.POST['travel_to']
         creator_id = request.session['user']['id']
         new_creator = User.objects.get(id = creator_id)
-        #first_participant = User.objects.get(creator = creator_id)
         
-    errors = User.objects.basic_validator(request.POST)
+        errors = Travel.objects.basic_validator(request.POST)
     
-    '''
-    if len(errors) > 0:
-        for llave, error_message in errors.items():
-            messages.error(request, error_message)
-        return redirect('/travelsadd')
-    '''
+        old_travels = Travel.objects.filter(destination= desti)
+        if len(old_travels) > 0:
+            messages.error(request,"This trip already exists.")
+            return redirect("/travelsadd")
+    
+        if len(errors) > 0:
+            for llave, error_message in errors.items():
+                messages.error(request, error_message)
+            return redirect('/travelsadd') 
 
-    try:
-        Travel.objects.create(destination = destination, description = description, travel_from = travel_from, travel_to = travel_to, creator = new_creator)
-    except IntegrityError:
-        messages.error(request, 'This travel already exist')
-        return redirect('/travelsadd')
+        try:
+            Travel.objects.create(destination = desti, description = description, travel_from = travel_from, travel_to = travel_to, creator = new_creator)
+        except IntegrityError:
+            messages.error(request, 'This travel already exist')
+            return redirect('/travelsadd')
 
     messages.success(request, 'Travel successfully created')
     return redirect('/travels')
     
 
+@login_required
 def destination(request, nam):
+    your_id = request.session['user']['id']
+    you = User.objects.get(id = your_id)
     this_travel = Travel.objects.get(id = int(nam))
-    this_travel_users = User.objects.filter(travels = this_travel)
-    #other_users_travels = [us for us in Travel.objects.all() if trip.id not in your_travels]
+    this_travel_travellers_without_you = User.objects.filter(travels = this_travel).exclude(id = your_id)
     context = {
         'this_travel': this_travel,
-        'this_travel_users': this_travel_users,
+        'this_travel_travellers_without_you': this_travel_travellers_without_you,
     }
     return render(request, 'destination.html', context)
 
+@login_required
 def cancel(request, nem):
     this_travel = Travel.objects.get(id = int(nem))
     your_id = request.session['user']['id'] 
-    you= User.objects.get(creator = your_id)
-    #travel_to_cancel = Travel.objects.filter(travellers = you)
+    you= User.objects.get(id = your_id)
+    this_travel_travellers = User.objects.filter(travels = this_travel)
+    if you not in this_travel_travellers:
+        messages.error(request,"You cannot cancel twice.")
+        return redirect('/travels')
     this_travel.travellers.remove(you)	
-    you.travels.remove(this_travel)
+    #you.travels.remove(this_travel)
     this_travel.save()
-    you.save()
+    #you.save()
     return redirect('/travels')
 
+@login_required
 def delete(request, nim):
     this_travel = Travel.objects.get(id = int(nim))
     del this_travel
     return redirect('/travels')
 
+@login_required
 def joining(request, nim):
     this_travel = Travel.objects.get(id = int(nim))
     your_id = request.session['user']['id'] 
     you= User.objects.get(id = your_id)
+    #si you ya esta como traveller de este viaje, tirarle error
+    this_travel_travellers = User.objects.filter(travels = this_travel)
+    
+    for guy in this_travel_travellers:
+        if guy.id == your_id:
+            messages.error(request,"You are already in.")
+            return redirect("/travels")
+        
     this_travel.travellers.add(you)
+    messages.success(request,"Now you are already in.")
+    this_travel.save()
     return redirect('/travels')
